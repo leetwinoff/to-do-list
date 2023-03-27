23f8126a331bd5181880f032fc4bb4d1811273a8
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 16:52:12 -0500

    Added path to TaskListView and TaskCreateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from todo.views import index
+from todo.views import index, TaskListView, TaskCreateView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks", TaskListView.as_view(), name="task-list")
+    path("tasks/create", TaskCreateView.as_view(), name="task-create")
 ]
 
 app_name = "todo"
