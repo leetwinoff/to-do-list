ba702ee7a6391e0a747d46b13e5cfb40c9cf55f4
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 17:17:20 -0500

    Added path to task update view

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from todo.views import index, TaskListView, TaskCreateView
+from todo.views import index, TaskListView, TaskCreateView, TagListView, TaskUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasks", TaskListView.as_view(), name="task-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
 ]
 
 app_name = "todo"
