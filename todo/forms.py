c2968eeddc7d8b3da22443c06a670fa46a1511f1
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 17:33:07 -0500

    Created TagForm

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
+        fields = "__all__"
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"
