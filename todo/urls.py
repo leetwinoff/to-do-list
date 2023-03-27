4064b3f5f14b57e3fbbec2d1679c08e3a398ed83
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 17:32:50 -0500

    Added path to tag create view

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from todo.views import index, TaskListView, TaskCreateView, TagListView, TaskUpdateView
+from todo.views import index, TaskListView, TaskCreateView, TagListView, TaskUpdateView, TagCreateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("tasks/create", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
     path("tags/", TagListView.as_view(), name="tags-list"),
+    path("tags/create", TagCreateView.as_view(), name="tags-create"),
 ]
 
 app_name = "todo"
