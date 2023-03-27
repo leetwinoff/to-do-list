2e46d8107effbcffcd3d52870b1447a86bc18946
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 16:44:36 -0500

    Added TaskListView

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo.models import Task
 
 
 def index(request):
     return render(request, "to_do_list/index.html")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
