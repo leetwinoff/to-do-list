7a1f8a0024bbeb81aa6b555bad066086fc5ca622
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 21:07:47 -0500

    Added path to task delete view to urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from todo.views import index, TaskListView, TaskCreateView, TagListView, TaskUpdateView, TagCreateView
+from todo.views import TaskListView, TaskCreateView, TagListView, TaskUpdateView, TagCreateView, TaskDeleteView
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("", TaskListView.as_view(), name="task-list"),
     path("tasks/create", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/create", TagCreateView.as_view(), name="tags-create"),
 ]
