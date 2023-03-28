dfdfab251951f3e366e48de358a7c470d0fab986
leetwinoff <leetwinoff92@gmail.com>
2023-03-27 21:14:29 -0500

    Added path for TagUpdateView and TagDeleteView to urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from todo.views import TaskListView, TaskCreateView, TagListView, TaskUpdateView, TagCreateView, TaskDeleteView
+from todo.views import TaskListView, TaskCreateView, TagListView, TaskUpdateView, TagCreateView, TaskDeleteView, \
+    TagDeleteView, TagUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
@@ -9,6 +10,8 @@ urlpatterns = [
     path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/create", TagCreateView.as_view(), name="tags-create"),
+    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
