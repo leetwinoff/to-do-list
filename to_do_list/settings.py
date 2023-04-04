783d392384441e45a4f85ea18150cac7551ac0f7
leetwinoff <leetwinoff92@gmail.com>
2023-04-04 14:14:42 -0500

    Deleted repeated rows

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -16,19 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = DJANGO_SECRET_KEY
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
